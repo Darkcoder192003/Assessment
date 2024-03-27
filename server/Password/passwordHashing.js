1668a25842b2f025ea143b34187f250ebf5fd9f9
Kuldeep Tiwari <kuldeeptiwari1992003@gmail.com>
2024-03-27 08:45:04 +0530

    Added Paswword Hasing

---
+++ server/Password/passwordHashing.js
@@ -0,0 +1,23 @@
+const bcrypt= require('bcrypt')
+
+const hashPassword =async(password)=>{
+    try {
+    
+        const salt = await bcrypt.genSalt(10);
+
+        const hash = await bcrypt.hash(password, salt);
+        return hash
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+const decryptPassword = async(password,hashedPassword)=>{
+    try{
+        return await bcrypt.compare(password,hashedPassword)
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
+module.exports = hashPassword
